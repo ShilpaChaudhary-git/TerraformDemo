669bcc0ab7a53e85536bd9b780e30ec1ba5f4e4a
ShilpaChaudhary-git <66250439+ShilpaChaudhary-git@users.noreply.github.com>
2020-11-17 17:18:57 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,17 @@
+# Create a new instance of the latest Ubuntu 20.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
+  region = "us-west-2"
   access_key = "key:AKIAVPK5R3Q6WB3EIZEW"
   secret_key = "key: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"
-  region " us-west-2"
 }
-resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.micro"
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
